594580067da16cb76df8cfcfbbc80535b957bb75
二货机器人 <smith3816@gmail.com>
2025-03-21 01:28:58 +0800

    chore: update ignore

--- bin/scaffold.js
+++ bin/scaffold.js
@@ -3,11 +3,15 @@ const path = require('path');
 const os = require('os');
 const { execSync } = require('child_process');
 
-const rootCwd = process.cwd();
+const rootCWD = process.cwd();
+
+// const PRESET_BRANCH = 'main';
+const PRESET_BRANCH = 'ChainReaction';
 
 // 获取 args 的最后一个参数
 const folderName = process.argv[process.argv.length - 1];
-console.log(`Clone playground: ${folderName}`);
+const targetFolder = path.join(`docs/playground/${folderName}`);
+console.log(`Clone playground: ${targetFolder}`);
 
 // 创建一个临时的文件夹
 const osTmpDir = os.tmpdir();
@@ -18,19 +22,28 @@ console.log(`Create tmp dir: ${tmpDir}`);
 process.chdir(tmpDir);
 
 // 执行 git clone 命令
-execSync(`git clone --no-checkout https://github.com/ant-design/x.git --depth=1 ./`, {
-  stdio: 'inherit',
-});
+console.log(`Target folder: ${targetFolder}`);
+execSync(
+  `git clone --no-checkout https://github.com/ant-design/x.git  --branch ${PRESET_BRANCH} --depth=1 ./`,
+  {
+    stdio: 'inherit',
+  },
+);
 execSync(`git sparse-checkout init --cone`, {
   stdio: 'inherit',
 });
-execSync(`git sparse-checkout set docs/playground`, {
+execSync(`git sparse-checkout set ${targetFolder}`, {
   stdio: 'inherit',
 });
-execSync(`git checkout main`, {
+execSync(`git checkout ${PRESET_BRANCH}`, {
   stdio: 'inherit',
 });
 
-// 获取当前系统的 tmp 文件夹目录
-
-// console.log(`tmpDirName: ${tmpDirName}`);
+// 把 targetFolder 移动到当前目录
+const cloneTargetFolder = path.join(rootCWD, '.scaffold');
+execSync(`rm -rf ${cloneTargetFolder}`, {
+  stdio: 'inherit',
+});
+execSync(`mv ${path.resolve(tmpDir, targetFolder)} ${cloneTargetFolder}`, {
+  stdio: 'inherit',
+});
