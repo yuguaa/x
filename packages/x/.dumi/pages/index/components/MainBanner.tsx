da60108817a0e4e8eb6d8acaa1ce1d6066557ce6
Mickey <951203214@qq.com>
2025-08-04 18:29:19 +0800

    doc: site

--- packages/x/.dumi/pages/index/components/MainBanner.tsx
+++ packages/x/.dumi/pages/index/components/MainBanner.tsx
@@ -11,6 +11,7 @@ import { getLocalizedPathname, isZhCN } from '../../../theme/utils';
 import Container from '../common/Container';
 import SiteContext from './SiteContext';
 import type { SiteContextProps } from './SiteContext';
+import Portal from './SceneIntroduction/Portal';
 
 const locales = {
   cn: {
@@ -45,6 +46,7 @@ const useStyle = createStyles(({ token, css }) => {
         height: calc(100vh - ${token.paddingLG}px);
       }
     `,
+
     background: css`
       width: 100%;
       height: 100vh;
@@ -56,15 +58,22 @@ const useStyle = createStyles(({ token, css }) => {
       height: 100%;
       max-height: calc(100vh - ${token.headerHeight * 2}px);
       position: relative;
+      display:flex;
+      justify-content:space-between;
+      padding-block-start:180px;
+    `,
+    x: css`
+     transition: rotate ${token.motionDurationMid};
+     width:85px;
+     margin-left: -10px;
+     display: inline-flex;
+     justify-content:center;     
     `,
     title: css`
       max-width: ${minBannerWidth}px;
-      position: absolute;
       top: 50%;
       inset-inline-start: 0;
-      transform: translateY(-50%);
       z-index: 1;
-
       @media only screen and (max-width: ${token.mobileMaxWidth}px) {
         width: 100%;
         display: flex;
@@ -74,6 +83,15 @@ const useStyle = createStyles(({ token, css }) => {
         text-align: center;
         gap: ${token.paddingXS}px;
       }
+      &:hover .x-hover{
+        rotate: -90deg;
+        color: transparent;
+        background-repeat:no-repeat;
+        background-size:contain;
+        background-position:center;
+        background-image:url('https://mdn.alipayobjects.com/huamei_lkxviz/afts/img/MGdkQ6iLuXEAAAAAQDAAAAgADtFMAQFr/original')
+      }
+
     `,
     lottie: css`
       position: absolute;
@@ -81,7 +99,6 @@ const useStyle = createStyles(({ token, css }) => {
       inset-inline-end: 0;
       transform: translate(${token.pcContainerMargin}px, -40%);
       z-index: 0;
-
       @media only screen and (max-width: ${token.mobileMaxWidth}px) {
         display: none;
       }
@@ -106,13 +123,11 @@ const useStyle = createStyles(({ token, css }) => {
       color: ${token.colorText};
       opacity: 0.65;
       margin: ${token.marginLG}px 0 ${token.marginLG * 2}px 0;
-      
     `,
     iAlphabet: css`
       position: relative;
       font-size: 60px;
       display: inline-block;
-
       @media only screen and (max-width: ${token.mobileMaxWidth}px) {
         transform: scale(0.7);
         top: 6px;
@@ -127,7 +142,6 @@ const useStyle = createStyles(({ token, css }) => {
       height: 22px;
       background: no-repeat center url('https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*RMOpRLHgA9wAAAAAAAAAAAAADgCCAQ/original');
       background-size: cover;
-
       &::before {
         content: '';
         width: 100%;
@@ -155,7 +169,6 @@ const useStyle = createStyles(({ token, css }) => {
       font-weight: 600;
       box-shadow: ${token.boxShadow};
       position: relative;
-
       @media only screen and (max-width: ${token.mobileMaxWidth}px) {
         padding: 0 ${token.paddingLG}px;
       }
@@ -164,7 +177,6 @@ const useStyle = createStyles(({ token, css }) => {
       background: linear-gradient(90deg, #c7deff 0%, #ffffffd9 76%);
       color: #14204c;
       position: relative;
-
       ::after {
         content: '';
         position: absolute;
@@ -178,7 +190,6 @@ const useStyle = createStyles(({ token, css }) => {
         z-index: -1;
         transition: opacity 0.2s;
       }
-
       :hover::after {
         opacity: 1;
       }
@@ -186,20 +197,17 @@ const useStyle = createStyles(({ token, css }) => {
     designBtn: css`
       background: #ffffff1a;
       backdrop-filter: blur(40px);
-
       &::after {
         content: '';
         width: 100%;
         height: 100%;
         box-sizing: border-box;
         border-radius: inherit;
-
         position: absolute;
         top: 0;
         bottom: 0;
         inset-inline-start: 0;
         inset-inline-end: 0;
-
         padding: ${token.lineWidth}px;
         background: linear-gradient(180deg, #ffffff26 0%, #ffffff00 100%);
         mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
@@ -296,17 +304,18 @@ const MainBanner: React.FC = () => {
     <section className={styles.banner}>
       <div ref={bgLottieRef} className={styles.background} />
       <Container className={styles.container}>
-        <div className={styles.title}>
-          <h1 className={styles.name}>
-            Ant Des
-            <span className={styles.iAlphabet}>
-              I<span className={styles.iAlphabetStar} />
-            </span>
-            gn X
-          </h1>
-          <h1 className={styles.name}>{locale.slogan}</h1>
-          <h5 className={styles.desc}>{locale.desc}</h5>
-
+        <div>
+          <div className={styles.title}>
+            <h1 className={styles.name}>
+              Ant Des
+              <span className={styles.iAlphabet}>
+                I<span className={styles.iAlphabetStar} />
+              </span>
+              gn   <div className={classnames(styles.x, 'x-hover')}>X</div>
+            </h1>
+            <h1 className={styles.name}>{locale.slogan}</h1>
+            <h5 className={styles.desc}>{locale.desc}</h5>
+          </div>
           <div className={styles.content}>
             <Link to={getLocalizedPathname('components/overview', isZhCN(pathname), search)}>
               <button type="button" className={classnames(styles.btn, styles.startBtn)}>
@@ -320,10 +329,12 @@ const MainBanner: React.FC = () => {
             </Link>
           </div>
         </div>
-        <div
+
+        <Portal />
+        {/* <div
           ref={ipLottieRef}
           className={classnames(styles.lottie, direction === 'rtl' && styles.lottie_rtl)}
-        />
+        /> */}
       </Container>
     </section>
   );
